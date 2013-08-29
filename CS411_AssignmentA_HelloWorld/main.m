044262bbcc7fe3ce41bba9877b684a557c87dbac
Iris Markofsky <MsDarkness@Iriss-MacBook-Pro.local>
2013-08-28 19:40:21 -0700

    Initial Commit

---
+++ CS411_AssignmentA_HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CS411_AssignmentA_HelloWorld
+//
+//  Created by Iris Markofsky on 8/28/13.
+//  Copyright (c) 2013 Iris Markofsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
